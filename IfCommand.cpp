cfce178d87d2847ae11e00ee513d3d3c57bd125d
Elad Israel <israelelad1@gmail.com>
2018-12-27 12:30:15 +0200

    Merge branch 'master' of /home/elad/CLionProjects/project1 with conflicts.

--- IfCommand.cpp
+++ IfCommand.cpp
@@ -29,8 +29,44 @@ void IfCommand::doCommand() {
 
     ConditionParser *conditionParser;
     conditionParser = new ConditionParser(left, oper, right, this->dataVars);
+    unordered_map<string, Command *> stringsToCommands = dataCommands->getStringsToCommands();
+    unsigned long oldIndex = index;
+
+
     if (conditionParser->checkCondition()) {
 
+        index = oldIndex;
+        // set the new index of dataCommands
+        this->dataCommands->setIndex(oldIndex);
+
+        auto it1 = this->dataCommands->getSeparated().begin();
+        it1 += index;
+        Command *command;
+        int bracesCounter = 0;
+        do {
+            if (this->dataCommands->getSeparated().at(index).find('{') != string::npos) {
+                bracesCounter++;
+                index++;
+                it1++;
+                // set the new index of dataCommands
+                this->dataCommands->setIndex(index);
+            } else if (this->dataCommands->getSeparated().at(index).find('}') != string::npos) {
+                bracesCounter--;
+                index++;
+                it1++;
+                // set the new index of dataCommands
+                this->dataCommands->setIndex(index);
+            }
+            auto it2 = stringsToCommands.find(*it1);
+            if (it2 == stringsToCommands.end()) {
+                it1++;
+                continue;
+            }
+            command = it2->second;
+            command->doCommand();
+            it1 += (this->dataCommands->getIndex() - index);
+            index = this->dataCommands->getIndex();
+        } while (bracesCounter != 0);
     }
 
     //TODO: index++ until we get to the }
